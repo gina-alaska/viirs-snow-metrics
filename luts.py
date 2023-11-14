b2b5361a823661adaeead0ecb778360ce37b3f73
Charlie Parr <cparr4@atlas07.snap.uaf.edu>
2023-11-14 12:29:24 -0900

    shrink dev mode time range for faster testing

--- luts.py
+++ luts.py
@@ -1,4 +1,4 @@
-"""Look-Up Tables for VIIRS Snow Metric Computations"""
+"""Look-Up Tables and Parameters for VIIRS Snow Metric Computations"""
 
 short_name = "VNP10A1F"
 
@@ -6,7 +6,7 @@ parameter_sets = {
     "dev_params": {
         "bbox": "-146,65,-145,66",
         "start_date": "2013-01-01T00:00:00Z",
-        "end_date": "2014-12-31T23:00:00Z",
+        "end_date": "2013-01-03T23:00:00Z",
     },
     "prod_params": {
         "bbox": "172,51,-130,72",
