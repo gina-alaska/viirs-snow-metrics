2b23f4ee4fa45528511825168273f0dce97cc458
Charles Parr <cparr4@elephant.gina.alaska.edu>
2023-12-15 08:44:59 -0900

    use shared raster write function

--- watermask.py
+++ watermask.py
@@ -1,13 +1,9 @@
-"""Create a mask of ocean and lake / inland water grid cells. These grid cells will be excluded from the snow metric computations. Each snow year will have an independent mask set to account for dynamic coastal and fluvial geomorphology."""
+"""Create a mask of grid cells that will be excluded from snow metric computations. This includes ocean and lake / inland water grid cells. These grid cells will be excluded from the snow metric computations. Each snow year will have an independent mask set to account for dynamic coastal and fluvial geomorphology."""
 
 import argparse
 import logging
-import pickle
-from pathlib import Path
 
 import xarray as xr
-import rasterio as rio
-import rioxarray
 import numpy as np
 import dask
 
@@ -87,46 +83,6 @@ def combine_masks(ocean_mask, inland_water_mask):
     return all_water_mask
 
 
-# def fetch_raster_profile(tile_id):
-#     """Fetch a raster profile to generate output mask rasters that match the downloaded NSIDC rasters.
-
-#     We load the GeoTIFF hash table to quicly extract a reference raster creation profile. Preserving these profiles should make the final alignment /
-#     mosaicking of the raster products a smoother process. We can also use this hash table to perform intermittent QC checks. For example, say FSD = 100 for some grid cell. We should then be able to map that value (100) to a date, then check the GeoTIFFs for that date, the date prior, and the date after, and observe the expected behavior (snow condition toggling from off to on).
-
-#     Args:
-#         tile_id (str): The tile identifier.
-
-#     Returns:
-#         dict: The raster profile.
-#     """
-
-#     with open("file_dict.pickle", "rb") as handle:
-#         geotiff_dict = pickle.load(handle)
-#     geotiff_reference = geotiff_dict[tile_id]["CGF_NDSI_Snow_Cover"][0]
-#     with rio.open(geotiff_reference) as src:
-#         out_profile = src.profile
-#     out_profile.update({"dtype": "int8"})
-#     out_profile.update({"nodata": 0})
-#     logging.info(f"Mask GeoTIFFs will use the raster creation profile {out_profile}.")
-#     return out_profile
-
-
-def write_mask_to_geotiff(tile_id, mask_name, out_profile, arr):
-    """Write a mask to a GeoTIFF file.
-
-    Args:
-        tile_id (str): The tile identifier.
-        mask_name (str): The name of the mask.
-        out_profile (dict): The raster profile.
-        arr (numpy.ndarray): The mask array.
-    """
-    out_fp = mask_dir / f"{tile_id}_{mask_name}.tif"
-    logging.info(f"Writing {mask_name} mask GeoTIFF to {out_fp}.")
-    with rio.open(out_fp, "w", **out_profile) as dst:
-        dst.update_tags(mask=mask_name)
-        dst.write(arr, 1)
-
-
 if __name__ == "__main__":
     logging.basicConfig(filename="mask.log", level=logging.INFO)
 
@@ -146,10 +102,26 @@ if __name__ == "__main__":
     mask_profile = fetch_raster_profile(
             tile_id, {"dtype": "int8", "nodata": 0}
         )
-    write_mask_to_geotiff(tile_id, "ocean", mask_profile, ocean_mask.values)
-    write_mask_to_geotiff(
-        tile_id, "inland_water", mask_profile, inland_water_mask.values
-    )
-    write_mask_to_geotiff(tile_id, "all_water", mask_profile, all_water_mask.values)
+    write_tagged_geotiff(
+        mask_dir,
+        tile_id,
+        "mask",
+        "ocean",
+        mask_profile,
+        ocean_mask.values)
+    write_tagged_geotiff(
+        mask_dir,
+        tile_id,
+        "mask",
+        "inland_water",
+        mask_profile,
+        inland_water_mask.values)
+    write_tagged_geotiff(
+        mask_dir,
+        tile_id,
+        "mask",
+        "all_water",
+        mask_profile,
+        all_water_mask.values)
 
     print("Masking Script Complete.")
