c97200ab047ef12ff23b3fef1c388a94daeeee23
Charles Parr <cparr4@elephant.gina.alaska.edu>
2023-12-15 09:42:18 -0900

    compute fsd, lsd, fss_range

--- compute_snow_metrics.py
+++ compute_snow_metrics.py
@@ -1,11 +1,9 @@
 """Compute the VIIRS snow metrics."""
 
-import pickle
 import logging
 import argparse
 import calendar
 from datetime import datetime, timedelta
-from pathlib import Path
 
 import xarray as xr
 import rasterio as rio
@@ -22,6 +20,17 @@ from shared_utils import (
 )
 
 
+def shift_to_day_of_snow_year_values(doy_arr):
+    """Day-of-snow-year corresponds to the familiar day of year (i.e., 1 to 365), but spans two calendar years (e.g., the first day of the second year is day 1 + 365 = 366). Because our snow year is defined as August 1 to July 31, possible values for day-of-snow-year are 213 to 577 (1 August is day of year 213; 31 July is day of year 212 + 365 = 577). When $SNOW_YEAR + 1 is a leap year, the maximum value may be 578."""
+
+    leap_year = calendar.isleap(int(SNOW_YEAR) + 1)
+    if not leap_year:
+        doy_arr += 212
+    else:
+        doy_arr += 213
+    return doy_arr
+
+
 def get_first_snow_day_array(chunked_cgf_snow_cover):
     """Compute first snow day (FSD) of the full snow season (FSS start day).
 
@@ -35,7 +44,7 @@ def get_first_snow_day_array(chunked_cgf_snow_cover):
     # or those values that are greater than 100 are masked out later on
     fsd_array = (chunked_cgf_snow_cover > snow_cover_threshold).argmax(dim="time")
     fsd_array += 1  # bumped this value by one, because argmax yields an index, and we index from zero, but don't want 0 values to represent a DOY in the output
-    return fsd_array
+    return shift_to_day_of_snow_year_values(fsd_array)
 
 
 def get_last_snow_day_array(chunked_cgf_snow_cover):
@@ -45,32 +54,27 @@ def get_last_snow_day_array(chunked_cgf_snow_cover):
     """
 
     ds_reverse_time = chunked_cgf_snow_cover.isel(time=slice(None, None, -1))
-
     last_occurrence_reverse = (ds_reverse_time > snow_cover_threshold).argmax(
         dim="time"
     )
-
     # must revert time indices back to the original order
     lsd_array = chunked_cgf_snow_cover.time.size - last_occurrence_reverse - 1
     # we could could omit the `- 1` above, but we'll be explicit and match the value bump used in FSD
     lsd_array += 1
-    return lsd_array
-
-
-def shift_to_day_of_snow_year_values(doy_arr):
-    """Day-of-snow-year corresponds to the familiar day of year (i.e., 1 to 365), but spans two calendar years (e.g., the first day of the second year is day 1 + 365 = 366). Because our snow year is defined as August 1 to July 31, possible values for day-of-snow-year are 213 to 577 (1 August is day of year 213; 31 July is day of year 212 + 365 = 577). When $SNOW_YEAR + 1 is a leap year, the maximum value may be 578."""
-
-    leap_year = calendar.isleap(SNOW_YEAR + 1)
-    if not leap_year:
-        doy_arr += 212
-    else:
-        doy_arr += 213
-    return doy_arr
+    return shift_to_day_of_snow_year_values(lsd_array)
 
 
 def compute_full_snow_season_range(lsd_array, fsd_array):
     """Compute range (i.e., length) of the full snow season."""
     return lsd_array - fsd_array - 1
+
+
+def apply_mask(mask_fp, array_to_mask):
+    """Mask out values from the snow metric array."""
+    with rio.open(mask_fp) as src:
+        mask_arr = src.read(1)
+    mask_applied = mask_arr * array_to_mask
+    return mask_applied
 
 
 if __name__ == "__main__":
@@ -83,23 +87,34 @@ if __name__ == "__main__":
     logging.info(f"Computing snow metrics for tile {tile_id}.")
 
     fp = preprocessed_dir / f"snow_year_{SNOW_YEAR}_{tile_id}.nc"
-    chunky_ds = open_preprocessed_dataset(fp, {"time": 52}, "CGF_NDSI_Snow_Cover")
-    fsd = get_first_snow_day_array(chunky_ds)
-    lsd = get_last_snow_day_array(chunky_ds)
-    fss_range = compute_full_snow_season_range(lsd, fsd)
+
+    chunky_ds = open_preprocessed_dataset(
+        fp, {"x": "auto", "y": "auto"}, "CGF_NDSI_Snow_Cover"
+    )
+
+    snow_metrics = dict()
+    snow_metrics.update({"first_snow_day": get_first_snow_day_array(chunky_ds)})
+    snow_metrics.update({"last_snow_day": get_last_snow_day_array(chunky_ds)})
+    snow_metrics.update(
+        {
+            "fss_range": compute_full_snow_season_range(
+                snow_metrics["last_snow_day"], snow_metrics["first_snow_day"]
+            )
+        }
+    )
 
     single_metric_profile = fetch_raster_profile(
         tile_id, {"dtype": "int16", "nodata": 0}
     )
+    for metric_name, metric_array in snow_metrics.items():
+        write_tagged_geotiff(
+            single_metric_dir,
+            tile_id,
+            "snow_metric",
+            metric_name,
+            single_metric_profile,
+            metric_array.compute().values.astype("int16")
+            # we don't actually have to call .compute(), but this coomunicates a chunked DataArray input and there is no performance penalty vs. just calling .values
+        )
 
-    write_tagged_geotiff(
-        single_metric_dir,
-        tile_id,
-        "snow metric",
-        "first_snow_day",
-        single_metric_profile,
-        fsd.compute().values.astype("int16")
-        # we don't actually have to call .compute(), but this reminds us we have a chunked DataArray input
-        # and there is no performance penalty vs. just calling .values
-    )
-    print("Preprocessing Script Complete.")
+    print("Snow Metric Computation Script Complete.")
