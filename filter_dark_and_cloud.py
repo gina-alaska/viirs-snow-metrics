d6efd8c4d9d359e0486eb9992a9a9adc2b5e5cfb
Charles Parr <cparr4@elephant.gina.alaska.edu>
2024-02-29 14:29:33 -0900

    extract values for dusk and dawn observations

--- filter_dark_and_cloud.py
+++ filter_dark_and_cloud.py
@@ -3,18 +3,16 @@
 import logging
 import argparse
 
-import rasterio as rio
 import numpy as np
 import xarray as xr
+import dask
 from dask.distributed import Client
 
 from config import preprocessed_dir, mask_dir, uncertainty_dir, SNOW_YEAR
-from luts import (
-    inv_cgf_codes,
-)
-import compute_snow_metrics as csm
+from luts import inv_cgf_codes
 from shared_utils import (
     open_preprocessed_dataset,
+    apply_threshold,
     fetch_raster_profile,
     apply_mask,
     write_tagged_geotiff,
@@ -27,7 +25,7 @@ def is_obscured(chunked_cgf_snow_cover, dark_source):
     The 'dark' is a cloud or winter darkness condition. These uncertainty sources will be handled in an identical fashion. Maybe there is a better word than dark (penumbra???) to describe this condition, but I can't think of it right now.
 
     Args:
-        chunked_cgf_snow_cover (xarray.DataArray): The chunked CGF snow cover data.
+        chunked_cgf_snow_cover (xr.DataArray): chunked CGF snow cover data.
         dark_source (str): one of 'Cloud' or 'Night'.
     Returns:
         xr.DataArray: Obscured grid cells.
@@ -36,41 +34,37 @@ def is_obscured(chunked_cgf_snow_cover, dark_source):
     return dark_on
 
 
-def count_darkness(chunked_cgf_snow_cover, dark_source):
+def count_darkness(dark_on):
     """Count the per-pixel occurrence of polar/winter darkness or cloud cover (in the initialization period) in the snow year.
 
     Args:
-        ds_chunked (xarray.Dataset): The chunked dataset.
-        dark_source (str): one of 'Cloud' or 'Night'.
+        dark_on (xr.Dataset): Obscured srid cells.
 
     Returns:
         xarray.DataArray: count of 'Cloud' or 'Night' values.
     """
 
-    logging.info(f"Counting occurence of `Night` values...")
-    darkness_count = (chunked_cgf_snow_cover == inv_cgf_codes[dark_source]).sum(
-        dim="time"
-    )
+    logging.info("Counting occurence of dark values...")
+    darkness_count = dark_on.sum(dim="time")
     return darkness_count
 
 
 def get_first_obscured_occurence_index(dark_on):
     """Get the first occurrence of the obscured condition.
 
-    Returns the first chronological occurrence of the obscured condition in the time dimension. Ranges between 0 and 365.
+    Returns the time index of the first chronological occurrence of the obscured condition. Ranges between 0 and 365.
     Args:
         dark_on (xr.DataArray): Obscured grid cells.
     Returns:
         xr.DataArray: The first occurrence of the obscured condition."""
     first_dark_index = dark_on.argmax(dim="time")
-    # first_dark_index = np.argmax(dark_on)
     return first_dark_index
 
 
 def get_last_obscured_occurence_index(dark_on):
     """Get the last occurrence of the obscured condition.
 
-    Returns the last chronological occurrence of the obscured condition in the time dimension. Ranges between 0 and 365.
+    Returns the time index of the last chronological occurrence of the obscured condition. Ranges between 0 and 365.
     Args:
         dark_on (xr.DataArray): Obscured grid cells.
     Returns:
@@ -82,118 +76,231 @@ def get_last_obscured_occurence_index(dark_on):
 
 
 def get_dusk_index(first_dark_index):
-    """Get the index of the last observation before the onset of the obscured condition.
+    """Get the time index of the last observation just before the onset of the obscured condition.
 
-    Ranges between -1 and 364. A value of -1 indicates that the first obscured condition occurred on the first day of the year. Again, 'dusk' might not be the best word but whatever.
+    Ranges between 0 and 364 (or 365 in a leap year). A value of 0 likely indicates that a grid cell did not experience the obscured condition at any point in the snow year.
+
     Args:
         first_dark_index (xr.DataArray): The first occurrence of the obscured condition.
     Returns:
         xr.DataArray: The index of the last observation before the onset of the obscured condition.
     """
     dusk_index = first_dark_index - 1
+    # handle edge case where obscured condition did not occur at all
+    # if we don't do this we hit an IndexError i.e. try to slice value at time index -1
+    dusk_index = dusk_index.where(dusk_index >= 0, 0)
     return dusk_index
 
 
-def get_dawn_index(last_dark_index):
-    """Get the index of the last observation before the onset of the obscured condition.
+def get_dawn_index(last_dark_index, len_time_index):
+    """Get the index of the first observation immediately after the obscured period.
 
-    Ranges between 1 and 366. As above re the choice of the word 'dawn'.
+    Index values range between 1 and 364 (or 365 in a leap year).
+
     Args:
         last_dark_index (xr.DataArray): The final occurrence of the obscured condition.
+        len_time_index (int): The length of the time index for any given snow year used to cap the dawn index value.
     Returns:
         xr.DataArray: The index of the first observation following the obscured period.
     """
     dawn_index = last_dark_index + 1
+    last_time_index = len_time_index - 1
+    # handle edge case where obscured condition did not occur at all
+    # if we don't do this we hit an IndexError i.e. try to slice value at time index 366
+    dawn_index = dawn_index.where(dawn_index <= last_time_index, last_time_index)
     return dawn_index
 
 
-def get_median_obscured_index(dusk, dawn):
+def get_median_obscured_index(dusk_index, dawn_index):
     """Get the median index of the obscured period.
 
     Basically - what is the midpoint of the dark or cloud period? We'll use this as a basis to fill values in the obscured period and determine first/last days of the full snow season.
 
     Args:
-        dusk (xr.DataArray): The index of the last observation before the onset of the obscured condition.
-        dawn (xr.DataArray): The index of the first observation following the obscured period.
+        dusk_index (xr.DataArray): The index of the last observation before the onset of the obscured condition.
+        dawn_index (xr.DataArray): The index of the first observation following the obscured period.
     Returns:
         xr.DataArray: The median index of the obscured period.
     """
-    return (dusk + dawn) // 2
+    return (dusk_index + dawn_index) // 2
 
 
-def get_snow_transition_case_dark(dusk, dawn, snow_on):
+@dask.delayed
+def get_dusk_observation(dusk_index, chunked_cgf_snow_cover):
+    """Get the value of the snow cover product at the onset of the obscured condition.
+
+    We delay this computation because index slicing the snowcover value at the time index basically requires an integer value.
+
+    Args:
+        chunked_cgf_snow_cover (xr.DataArray): chunked CGF snow cover data.
+        dusk_index (xr.DataArray): index of the last observation before the onset of the obscured condition.
+    Returns:
+        xr.DataArray: Value of observation prior to the obscured period.
+    """
+    dusk_observation = chunked_cgf_snow_cover.isel(time=dusk_index)
+    return dusk_observation
+
+
+@dask.delayed
+def get_dawn_observation(dawn_index, chunked_cgf_snow_cover):
+    """Get the value of the snow cover product immediately after the obscured condition.
+
+    We delay this computation because index slicing the snowcover value at the time index basically requires an integer value.
+
+    Args:
+        chunked_cgf_snow_cover (xr.DataArray): chunked CGF snow cover data.
+        dawn_index (xr.DataArray): index of the first observation following the obscured period.
+    Returns:
+        xr.DataArray: value of the first observation following the obscured period.
+    """
+    dawn_observation = chunked_cgf_snow_cover.isel(time=dawn_index)
+    return dawn_observation
+
+
+def is_snow_on_at_dusk(dusk_observation):
+    """Determine if snow is "on" at dusk.
+
+    Args:
+        dusk_observation (xr.DataArray): Value of the last observation before the onset of the obscured condition.
+    Returns:
+        xr.DataArray: boolean array indicating if snow is on at dusk.
+    """
+    return apply_threshold(dusk_observation)
+
+
+def is_snow_on_at_dawn(dawn_observation):
+    """Determine if snow is "on" at dawn.
+
+    Args:
+        dawn_observation (xr.DataArray): value of the first observation following the obscured period.
+    Returns:
+        xr.DataArray: boolean array indicating if snow is on at dawn.
+    """
+    return apply_threshold(dawn_observation)
+
+
+@dask.delayed
+def get_snow_transition_cases(snow_is_on_at_dusk, snow_is_on_at_dawn):
     """Determine the snow transition case during the obscured period.
 
-    This functions determines what, if anything, happened to the binary snow-state between the last observation before the onset of the obscured condition and the first observation following the obscured period. It returns three boolean arrays: one for no change in snow condition, one for snow flipping from off to on, and one for snow flipping from on to off.
 
-    Args:
-        dusk (xr.DataArray): The index of the last observation before the onset of the obscured condition.
-        dawn (xr.DataArray): The index of the first observation following the obscured period.
-        snow_on (xr.DataArray): The binary snow-state.
-    Returns:
-        tuple: Three boolean arrays indicating no change in snow condition, snow flipping from off to on, and snow flipping from on to off.
+    We delay this computation because `np.select` requires boolean arrays. Determine what, if anything, happened to the binary (on or off) snow state between the dusk observation the dawn observation. This information will be used to map different forward and backward data filling strategies for cloud or winter darkness conditions.
     """
-    # some handling for edge cases where the obscured (cloud or night) condition did not occur at all that can otherwise toss an IndexError when we try to grab the value of `snow_on` at time index -1 for example
-    try:
-        last_obs_before_darkness = snow_on.isel(time=dusk)
-    except:
-        # handle case where first_dark is first_day_of_year
-        last_obs_before_darkness = snow_on.isel(time=0)
-    try:
-        first_obs_after_darkness = snow_on.isel(time=dawn)
-    except:
-        # handle case where last_dark is last_day_of_year
-        first_obs_after_darkness = snow_on.isel(time=365)
-
     # no change in snow condition during the obscured period
     # return true when conditions are identical before and after obscured period
-    snow_did_not_flip = ~(last_obs_before_darkness ^ first_obs_after_darkness)
-    # first snow happened during the obscured period, this is probably common
-    snow_flipped_off_to_on = (last_obs_before_darkness == False) & (
-        first_obs_after_darkness == True
+    snow_did_not_flip = ~(snow_is_on_at_dusk ^ snow_is_on_at_dawn)
+    # first snow happened during the obscured period
+    snow_flipped_off_to_on = (snow_is_on_at_dusk == False) & (
+        snow_is_on_at_dawn == True
     )
-    # snow went away during the obscured period, probably less common
-    snow_flipped_on_to_off = (last_obs_before_darkness == True) & (
-        first_obs_after_darkness == False
+    # snow went away during the obscured period
+    snow_flipped_on_to_off = (snow_is_on_at_dusk == True) & (
+        snow_is_on_at_dawn == False
     )
-    return (
-        snow_did_not_flip.compute(),
-        snow_flipped_off_to_on.compute(),
-        snow_flipped_on_to_off.compute(),
-    )
-
-
-def compute_snow_darkness_transitions(chunked_cgf_snow_cover, obscured_source):
-    """Compute snow transitions during the obscured period.
-
-    This function orchestrates the computation of snow transitions during the obscured period. It returns three boolean arrays: one for no change in snow condition, one for snow flipping from off to on, and one for snow flipping from on to off. It also returns the dusk, dawn, and median obscured indices. These data will be used to map different forward and backward data filling strategies for cloudy or winter darkness conditions.
-
-    Args:
-        chunked_cgf_snow_cover (xarray.DataArray): The chunked CGF snow cover data.
-    Returns:
-        tuple: Six arrays in total. Three boolean arrays indicating no change in snow condition, snow flipping from off to on, and snow flipping from on to off. Also returns 'dusk', 'dawn', and median obscured indices.
-    """
-
-    dark_on = is_obscured(chunked_cgf_snow_cover, obscured_source)
-
-    first_dark_index = get_first_obscured_occurence_index(dark_on)
-    last_dark_index = get_last_obscured_occurence_index(dark_on)
-    dusk = get_dusk_index(first_dark_index).compute()
-    dawn = get_dawn_index(last_dark_index).compute()
-    median_obscured_index = get_median_obscured_index(dusk, dawn)
-
-    snow_is_on = csm.apply_threshold(chunked_cgf_snow_cover)
-    get_snow_transition_case_dark(dusk, dawn, snow_is_on)
-    snow_did_not_flip, snow_flipped_off_to_on, snow_flipped_on_to_off = (
-        get_snow_transition_case_dark(dusk, dawn, snow_is_on)
-    )
-    return (
+    # return an array where the value is 1 if snow did not flip
+    # or 2 if snow flipped off to on
+    # or 3 if snow flipped on to off
+    transition_cases = [
         snow_did_not_flip,
         snow_flipped_off_to_on,
         snow_flipped_on_to_off,
-        dusk,
-        dawn,
-        median_obscured_index,
+    ]
+    # mapped to the above
+    numeric_transition_cases = [1, 2, 3]
+    snow_transition_cases = np.select(
+        transition_cases, numeric_transition_cases, default=0
+    )
+    return snow_transition_cases
+
+
+def create_dark_metric_computation(dark_tag, dark_is_on, chunky_ds):
+    """Create a dictionary of delayed computations for darkness metrics.
+
+    This is a look-up-table for how each metric gets computed.
+
+    Args:
+        dark_tag (str): One of 'cloud' or 'night'.
+        dark_is_on (xr.DataArray): The obscured condition.
+        chunky_ds (xr.Dataset): The chunked CGF snow cover data.
+    Returns:
+        dict: A dictionary of delayed computations for darkness metrics."""
+
+    dark_metrics = dict()
+
+    dark_metrics.update({f"{dark_tag}_obscured_day_count": count_darkness(dark_is_on)})
+    dark_metrics.update(
+        {
+            f"dusk_index_of_last_obs_prior_to_{dark_tag}": get_dusk_index(
+                get_first_obscured_occurence_index(dark_is_on)
+            )
+        }
+    )
+    dark_metrics.update(
+        {
+            f"dawn_index_of_first_obs_after_{dark_tag}": get_dawn_index(
+                get_last_obscured_occurence_index(dark_is_on),
+                chunky_ds.time.size,
+            )
+        }
+    )
+    dark_metrics.update(
+        {
+            f"median_index_of_{dark_tag}_period": get_median_obscured_index(
+                dark_metrics[f"dusk_index_of_last_obs_prior_to_{dark_tag}"],
+                dark_metrics[f"dawn_index_of_first_obs_after_{dark_tag}"],
+            )
+        }
+    )
+    dark_metrics.update(
+        {
+            f"value_at_{dark_tag}_dusk": get_dusk_observation(
+                dark_metrics[f"dusk_index_of_last_obs_prior_to_{dark_tag}"],
+                chunky_ds,
+            ),
+        }
+    )
+    dark_metrics.update(
+        {
+            f"value_at_{dark_tag}_dawn": get_dawn_observation(
+                dark_metrics[f"dawn_index_of_first_obs_after_{dark_tag}"],
+                chunky_ds,
+            ),
+        }
+    )
+    dark_metrics.update(
+        {
+            f"snow_is_on_at_{dark_tag}_dusk": is_snow_on_at_dusk(
+                dark_metrics[f"value_at_{dark_tag}_dusk"]
+            )
+        }
+    )
+    dark_metrics.update(
+        {
+            f"snow_is_on_at_{dark_tag}_dawn": is_snow_on_at_dawn(
+                dark_metrics[f"value_at_{dark_tag}_dawn"]
+            )
+        }
+    )
+    dark_metrics.update(
+        {
+            f"snow_transition_cases_{dark_tag}": get_snow_transition_cases(
+                dark_metrics[f"snow_is_on_at_{dark_tag}_dusk"],
+                dark_metrics[f"snow_is_on_at_{dark_tag}_dawn"],
+            )
+        }
+    )
+    return dark_metrics
+
+
+def write_dark_metric(dark_metric_name, computation_di):
+    dark_metric_arr = computation_di[dark_metric_name].compute()
+    write_tagged_geotiff(
+        uncertainty_dir,
+        tile_id,
+        "",
+        dark_metric_name,
+        out_profile,
+        apply_mask(combined_mask, dark_metric_arr).astype("int16"),
     )
 
 
@@ -205,48 +312,34 @@ if __name__ == "__main__":
     tile_id = args.tile_id
     logging.info(f"Filtering winter darkness and cloud cover for tile {tile_id}.")
 
-    client = Client()
-
+    client = Client(memory_limit="64GiB", timeout="60s")  # mem per Dask worker
+    # intialize input and output parameters
     fp = preprocessed_dir / f"snow_year_{SNOW_YEAR}_{tile_id}.nc"
-    chunky_ds = open_preprocessed_dataset(
+    ds = open_preprocessed_dataset(
         fp, {"x": "auto", "y": "auto"}, "CGF_NDSI_Snow_Cover"
     )
     out_profile = fetch_raster_profile(tile_id, {"dtype": "int16", "nodata": 0})
     combined_mask = mask_dir / f"{tile_id}__mask_combined_{SNOW_YEAR}.tif"
+
     for darkness_source in ["Night"]:  # add "Cloud" back in here
-        # (
-        #     snow_did_not_flip,
-        #     snow_flipped_off_to_on,
-        #     snow_flipped_on_to_off,
-        #     dusk,
-        #     dawn,
-        #     median_obscured_index,
-        # ) = compute_snow_darkness_transitions(chunky_ds, darkness_source)
-        snow_darkness_transitions = compute_snow_darkness_transitions(
-            chunky_ds, darkness_source
-        )
-        dark_count = count_darkness(chunky_ds, darkness_source)
+        tag = darkness_source.lower()
+        darkness_is_on = is_obscured(ds, darkness_source)
+        dark_computation_di = create_dark_metric_computation(tag, darkness_is_on, ds)
 
-        dark_label = darkness_source.lower()
-        tiff_labels = [
-            f"snow_did_not_flip_during_{dark_label}",
-            f"snow_flipped_off_to_on_during_{dark_label}",
-            f"snow_flipped_on_to_off_during_{dark_label}",
-            f"dusk_index_of_last_obs_prior_to_{dark_label}",
-            f"dawn_index_of_last_obs_prior_to_{dark_label}",
-            f"{dark_label}_darkness_count_of_days",
-        ]
-        for label, arr in zip(
-            tiff_labels, list(snow_darkness_transitions) + [dark_count]
-        ):
-            write_tagged_geotiff(
-                uncertainty_dir,
-                tile_id,
-                "",
-                label,
-                out_profile,
-                apply_mask(combined_mask, arr),
-            )
-
+        # these are relatively rapid computes
+        # write_dark_metric(f"{tag}_obscured_day_count", dark_computation_di)
+        # write_dark_metric(f"dusk_index_of_last_obs_prior_to_{tag}", dark_computation_di)
+        # write_dark_metric(f"dawn_index_of_first_obs_after_{tag}", dark_computation_di)
+        # write_dark_metric(f"median_index_of_{tag}_period", dark_computation_di)
+        # more memory intensive because they rely on computing the above indices
+        # write_dark_metric(f"value_at_{tag}_dusk", dark_computation_di)
+        # write_dark_metric(f"value_at_{tag}_dawn", dark_computation_di)
+        # write_dark_metric(f"snow_is_on_at_{tag}_dusk", dark_computation_di)
+        # write_dark_metric(f"snow_is_on_at_{tag}_dawn", dark_computation_di)
+        write_dark_metric(f"snow_transition_cases_{tag}", dark_computation_di)
     client.close()
     print("Cloud and Darkness Filtering Script Complete.")
+
+#     f"snow_did_not_flip_during_{dark_label}",
+#     f"snow_flipped_off_to_on_during_{dark_label}",
+#     f"snow_flipped_on_to_off_during_{dark_label}",
