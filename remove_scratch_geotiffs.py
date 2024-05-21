c64bfe8fb3c20701ec5ce06b6e578adaffd7981f
Charles Parr <cparr4@elephant.gina.alaska.edu>
2024-05-21 08:40:44 -0800

    initial commit

---
+++ remove_scratch_geotiffs.py
@@ -0,0 +1,18 @@
+import os
+from pathlib import Path
+from config import snow_year_scratch_dir
+
+geotiff_files = list(snow_year_scratch_dir.rglob("*.tif"))
+
+# prompt to confirm deletion
+print(
+    f"Are you sure you want to delete {len(geotiff_files)} GeoTIFF files from {snow_year_scratch_dir}? (yes/no)"
+)
+confirmation = input()
+
+if confirmation.lower() == "yes":
+    for file in geotiff_files:
+        os.remove(file)
+    print(f"Deleted {len(geotiff_files)} GeoTIFF files from {snow_year_scratch_dir}.")
+else:
+    print("Operation cancelled.")
