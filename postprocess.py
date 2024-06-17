24c1cacd971ce766f9af6c917d8008028bc1eef4
Charles Parr <cparr4@elephant.gina.alaska.edu>
2024-06-17 14:34:11 -0800

    log to $HOME

--- postprocess.py
+++ postprocess.py
@@ -141,7 +141,8 @@ def merge_geotiffs(file_list, output_file):
 
 
 if __name__ == "__main__":
-    logging.basicConfig(filename="postprocess.log", level=logging.INFO)
+    log_file_path = os.path.join(os.path.expanduser("~"), "postprocess.log")
+    logging.basicConfig(filename=log_file_path, level=logging.INFO)
 
     for tiff_flavor in tiff_path_dict.keys():
         logging.info(f"Reprojecting {tiff_flavor} to EPSG:3338...")
