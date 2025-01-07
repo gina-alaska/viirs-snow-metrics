55aef91a2f4a3de666364a3b42979587505c2b98
ojlarson <ojlarson@alaska.edu>
2025-01-07 11:53:48 -0900

    tested unique tile parsing

--- tests/download_tests.py
+++ tests/download_tests.py
@@ -1,11 +1,13 @@
 import unittest
 from pathlib import Path
 import shutil
+import time
 
 from download import generate_monthly_dl_chunks
 from NSIDC_download import cmr_search, cmr_download
 from luts import parameter_sets, needed_tile_ids
 from h5_utils import parse_tile_h5
+
 
 class UnitTest(unittest.TestCase):
 
@@ -18,7 +20,6 @@ class UnitTest(unittest.TestCase):
 
     def test_generate_monthly_dl_chunks(self):
         snow_year_chunks = generate_monthly_dl_chunks(2023)
-        print("SNC", snow_year_chunks)
         self.assertEqual(len(snow_year_chunks), 12)
 
     def test_cms_search(self):
@@ -35,7 +36,7 @@ class UnitTest(unittest.TestCase):
                 bounding_box=bounding_box
             )
         self.assertEqual(len(url_list), 360)
-        self.assertIn('VNP10A1F.A2024092.h08v03.002.2024093202023.h5', url_list[0])
+        self.assertIn('VNP10A1F', url_list[0])
         short_name = 'VJ110a1f'
         url_list = cmr_search(
                 short_name,
@@ -44,7 +45,7 @@ class UnitTest(unittest.TestCase):
                 bounding_box=bounding_box
             )
         self.assertEqual(len(url_list), 360)
-        self.assertIn('VJ110A1F.A2024092.h08v03.002.2024103112026.h5', url_list[0])
+        self.assertIn('VJ110A1F', url_list[0])
 
     def test_cmr_download(self):
         snow_year_chunks = generate_monthly_dl_chunks(2023)
@@ -76,8 +77,23 @@ class UnitTest(unittest.TestCase):
                 bounding_box=bounding_box
             )
         unique_tiles = set([parse_tile_h5(Path(x)) for x in url_list])
+        start_time = time.time()
+        good_tiles = [url for url in url_list if parse_tile_h5(Path(url)) in needed_tile_ids and Path(url).suffix != '.xml']
+        print(f"List comprehension: {time.time() - start_time:.4f} seconds")
+
+        start_time = time.time()
+        good_tiles = []
+        for x in url_list:
+            path_x = Path(x)
+            if parse_tile_h5(path_x) in needed_tile_ids and path_x.suffix != '.xml':
+                good_tiles.append(x)
+        print(f"Manual append: {time.time() - start_time:.4f} seconds")
+
+        unique_tiles = set([parse_tile_h5(Path(x)) for x in good_tiles])
+        print(unique_tiles)
         self.assertEqual(unique_tiles, needed_tile_ids)
-        
+
+
 
 
     def tearDown(self):
