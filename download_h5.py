68b2e3f776f8f36752dfb7ada45b3a39faeac051
ojlarson <ojlarson@alaska.edu>
2025-08-27 11:41:27 -0800

    add version handling

--- download_h5.py
+++ download_h5.py
@@ -1,23 +1,6 @@
-"""Download VIIRS Data from the NSIDC DAAC. See https://github.com/nsidc/NSIDC-Data-Access-Notebook for reference. This module uses large amounts of code from notebook examples within that repo."""
-
-import requests
-import getpass
-import json
-import zipfile
-import io
-import math
+"""Download VIIRS Data from the NSIDC DAAC. See https://earthaccess.readthedocs.io/en/stable/ for reference."""
 import os
-import shutil
-import re
-import sys
-import time
 import logging
-import calendar
-import argparse
-from statistics import mean
-from xml.etree import ElementTree as ET
-from datetime import datetime, timedelta
-from pathlib import Path
 import earthaccess
 
 from luts import short_name, needed_tile_ids
@@ -32,17 +15,27 @@ def main(short_name):
 
     for time_chunk in snow_year_chunks:
         print(f"Starting download for {time_chunk}.")
+        datasets = earthaccess.search_datasets(
+            short_name=short_name,
+        )
+        if len(datasets) == 1:
+            version = datasets[0]['umm']['Version']
+        else:
+            versions = [int(ds['umm']['Version']) for ds in datasets if 'umm' in ds and 'Version' in ds['umm']]
+            version = str(max(versions)) if versions else None
         url_list = earthaccess.search_data(
             short_name=short_name,
             bounding_box=tuple(map(int, viirs_params["bbox"].split(","))),
             temporal=(time_chunk[0], time_chunk[1]),
             # daac='NSIDC', # Seems to work without this - but possible specifying daac is needed to avoid duplicates for some years/data
-            version=2,
+            version=version,
         )
         earthaccess.download(url_list, local_path=snow_year_input_dir)
 
 
 if __name__ == "__main__":
+    import argparse
+    
     log_file_path = os.path.join(os.path.expanduser("~"), "input_data_download.log")
     logging.basicConfig(
         format="%(asctime)s - %(levelname)s - %(message)s",
