454a85d8e5b31f7ba4ddc3cb3cf4c2d1e091b44d
ojlarson <ojlarson@alaska.edu>
2025-08-28 10:42:07 -0800

    bring in h5 methods and delete h5 script. Arg added for formet, defaults to h5

--- download.py
+++ download.py
@@ -17,7 +17,7 @@ from statistics import mean
 from xml.etree import ElementTree as ET
 from datetime import datetime, timedelta
 
-from luts import short_name
+from luts import short_name, needed_tile_ids
 from config import viirs_params, snow_year_input_dir, SNOW_YEAR
 
 
@@ -470,16 +470,16 @@ def validate_download(dl_path, number_granules_requested):
         logging.info("Downloaded file count matches expectations.")
 
 
-if __name__ == "__main__":
-    log_file_path = os.path.join(os.path.expanduser("~"), "input_data_download.log")
-    logging.basicConfig(
-        format="%(asctime)s - %(levelname)s - %(message)s",
-        filename=log_file_path,
-        level=logging.INFO,
-    )
+def download_tif():
+    """Wrapper function to download GeoTIFF files. Uses methods for conversion and reprojection that will not be available for future datasets.
+    These methods are retained for legacy support and may cease to function properly in the future.
 
-    wipe_old_downloads(snow_year_input_dir)
+    Args:
+        None
 
+    Returns:
+        None
+    """
     v = check_data_version(short_name)
     ref_tiles = determine_tiles_for_bbox(viirs_params["bbox"])
     snow_year_chunks = generate_monthly_dl_chunks(int(SNOW_YEAR))
@@ -517,4 +517,96 @@ if __name__ == "__main__":
         validate_download(snow_year_input_dir, len(granule_list))
 
     api_session.close()
+
+
+def download_h5(short_name=short_name):
+    """Wrapper function to download HDF5 files from the NSIDC DAAC using earthaccess API.
+    This is the preferred method for downloading VIIRS data.
+
+    Args:
+        short_name (str): The short name of the dataset. Default is stored in luts.py.
+
+    Returns:
+        None
+    """
+    import earthaccess
+
+    earthaccess.login(strategy="interactive")
+    snow_year_chunks = generate_monthly_dl_chunks(int(SNOW_YEAR))
+
+    for time_chunk in snow_year_chunks:
+        logging.info(f"Starting download for {time_chunk}.")
+        datasets = earthaccess.search_datasets(
+            short_name=short_name,
+        )
+        if len(datasets) == 1:
+            version = int(datasets[0]["umm"]["Version"])
+        else:
+            versions = [
+                int(ds["umm"]["Version"])
+                for ds in datasets
+                if "umm" in ds and "Version" in ds["umm"]
+            ]
+            version = max(versions) if versions else None
+        url_list = earthaccess.search_data(
+            short_name=short_name,
+            bounding_box=tuple(map(int, viirs_params["bbox"].split(","))),
+            temporal=(time_chunk[0], time_chunk[1]),
+            # daac='NSIDC', # Seems to work without this - but possible specifying daac is needed to avoid duplicates for some years/data
+            version=int(version),
+        )
+        if url_list:
+            # Filter to only needed tiles
+            url_list = [
+                url
+                for url in url_list
+                for tile_id in needed_tile_ids
+                if tile_id in url["umm"]["GranuleUR"]
+            ]
+            logging.info(
+                f"{len(url_list)} files match needed tiles for {time_chunk}. Downloading..."
+            )
+            if url_list:
+                earthaccess.download(url_list, local_path=snow_year_input_dir)
+            else:
+                logging.info(f"No files match needed tiles for {time_chunk}. Exiting.")
+                exit(1)
+        else:
+            logging.info(f"No files found for {time_chunk}. Exiting.")
+            exit(1)
+
+
+if __name__ == "__main__":
+    import argparse
+
+    log_file_path = os.path.join(os.path.expanduser("~"), "input_data_download.log")
+    logging.basicConfig(
+        format="%(asctime)s - %(levelname)s - %(message)s",
+        filename=log_file_path,
+        level=logging.INFO,
+    )
+    parser = argparse.ArgumentParser(description="Download Script.")
+
+    parser.add_argument(
+        "--format",
+        "-f",
+        choices=["tif", "h5"],
+        default="h5",
+        help="Download/input File format: Older processing methods use tif, newer uses h5. Default is h5.",
+    )
+    parser.add_argument(
+        "--short_name",
+        type=str,
+        help="Dataset short name - will overwrite short_name from luts if used. Only usable with h5 download methods.",
+    )
+    args = parser.parse_args()
+    if args.short_name:
+        short_name = args.short_name
+
+    wipe_old_downloads(snow_year_input_dir)
+
+    if args.format == "tif":
+        download_tif()
+    else:
+        download_h5(short_name=short_name)
     print("Download Script Complete.")
